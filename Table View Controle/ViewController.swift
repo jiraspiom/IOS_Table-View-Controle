0c8a6fc3092e5e8492d07ef9494d43d3ae6a3505
gilmar <jiraspiom@yahoo.com.br>
2018-10-25 08:22:33 -0300

    Initial Commit

---
+++ Table View Controle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table View Controle
+//
+//  Created by Gilmar Borges on 25/10/18.
+//  Copyright © 2018 alalgi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
