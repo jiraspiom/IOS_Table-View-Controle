fc7d8d57a80edee1945679976a02462ba9d95cb1
gilmar <jiraspiom@yahoo.com.br>
2018-10-25 08:39:57 -0300

    retorno 1 sessão, e adicionado um arry para armazenar os dados na table

--- Table View Controle/ViewController.swift
+++ Table View Controle/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var dados : [String] = ["metal gears", "super metroid", "god of war"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
+    
 
 }
 
