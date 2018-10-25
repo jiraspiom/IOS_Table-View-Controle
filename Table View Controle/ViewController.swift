6aa8fd01bd8cefb744e7c64af00992aa8993ca6d
gilmar <jiraspiom@yahoo.com.br>
2018-10-25 08:44:07 -0300

    Quantas linha sera exibidas

--- Table View Controle/ViewController.swift
+++ Table View Controle/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+//alterar a heranca para table
 class ViewController: UITableViewController {
 
     var dados : [String] = ["metal gears", "super metroid", "god of war"]
@@ -17,11 +18,16 @@ class ViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    //quantas sessões ira ser criada
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
     
+    //quantas linha vou exibir
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //será retornado o tamanho de retitos no array
+        return dados.count
+    }
 
 }
 
